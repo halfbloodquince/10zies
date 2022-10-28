1c9c8a57e0c012a062ed394a32aa989dfa1815cb
Matt Woods <mattwoodsmaths@gmail.com>
2022-10-28 09:45:52 +0200

    rotating dice

--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -1,10 +1,28 @@
+import {motion} from "framer-motion"
+
+
 export default function Die(props) {
+
+
 
     const dieIsHeldStyle = { backgroundColor: props.isHeld ? "#59E391": "#FFFFFF"}
  
 
     return (
-    <div style={dieIsHeldStyle} className="die" onClick={props.holdDice}>
+    <motion.div
+        transition={{
+            ease: [0.97, 0.97, 0.93, 0.97]
+      }} 
+        initial={{rotate:180}} 
+        animate={{
+        x: 0,
+        y: 0,
+        scale: 1,
+        rotate: 0,
+      }} 
+        style={{...dieIsHeldStyle}} 
+        className="die" 
+        onClick={props.holdDice}>
         <h2 className="die--num">{props.value}</h2>
-    </div>)
+    </motion.div>)
 }
