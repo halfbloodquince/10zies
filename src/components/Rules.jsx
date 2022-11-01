e6167960a90bddaf17b22d828fec602d87be0bf1
Matt Woods <mattwoodsmaths@gmail.com>
2022-11-01 15:44:50 +0100

    rules II

--- src/components/Rules.jsx
+++ src/components/Rules.jsx
@@ -2,10 +2,10 @@ import {motion} from "framer-motion"
 
 export default function Rules(props) {
 
-    const buttonClick = props.rulesOn ? "-200%" : "-30%"
+    const buttonClick = props.rulesOn ? "-205%" : "-30%"
 
     return (
-        <motion.div  animate={{xlinkRole: 0,y: buttonClick,scale: 1,rotate: 0,}} transition={{ ease: "easeOut", duration: 3 }} 
+        <motion.div  animate={{xlinkRole: 0,y: buttonClick,scale: 1,rotate: 0,}} transition={{ ease: "easeOut", duration: 2 }} 
         initial={false} className="rules--container">
             <div className="rules--text">
                 <p>Roll until all numbers are the same.</p>
